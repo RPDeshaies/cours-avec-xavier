9c313af4c796d18ad549a032ee3ebe7367a059cf
Rene-Pier Deshaies-Gelinas <rene-pier.deshaies-gelinas@plusgrade.com>
2018-12-15 12:49:15 -0500

    format

--- cours/1/main.js
+++ cours/1/main.js
@@ -1,20 +1,19 @@
 function additon(firstNumber, secondNumber) {
-    const resultat = firstNumber + secondNumber;
-    return resultat;
-  }
-  
-  function division(chiffreADiviserParCent) {
-    const resultatDivison = chiffreADiviserParCent / 100;
-    return resultatDivison;
-  }
-  
-  function main(firstNumber, secondNumber) {
-    const resultAtaddition1 = additon(firstNumber, secondNumber);
-    const resultatDivison = division(resultAtaddition1);
-    return resultatDivison;
-  }
-  
-  const resultat = main(10, 10);
-  
-  console.log(resultat);
-  
+  const resultat = firstNumber + secondNumber;
+  return resultat;
+}
+
+function division(chiffreADiviserParCent) {
+  const resultatDivison = chiffreADiviserParCent / 100;
+  return resultatDivison;
+}
+
+function main(firstNumber, secondNumber) {
+  const resultAtaddition1 = additon(firstNumber, secondNumber);
+  const resultatDivison = division(resultAtaddition1);
+  return resultatDivison;
+}
+
+const resultat = main(10, 10);
+
+console.log(resultat);
